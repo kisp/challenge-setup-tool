ddd9cfb2ee3e16bf76275a97a156adc92bacbef2
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-03-20 10:33:30 +0100

    Improvements (#2)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./main.js"],
+  content: ["./index.html", "./main.js", "./icons.js"],
   theme: {
     extend: {},
   },
